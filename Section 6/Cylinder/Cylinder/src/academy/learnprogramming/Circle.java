2e2d2c3f6186c5ea5b537128f63dc7b129d3e64d
Ivan Logutov <foreverivan2004@gmail.com>
2022-10-08 17:49:34 +0300

    Adding Circle Challenge (6 section, 35 ex)

---
+++ Section 6/Cylinder/Cylinder/src/academy/learnprogramming/Circle.java
@@ -0,0 +1,19 @@
+package academy.learnprogramming;
+
+public class Circle {
+
+    private double radius;
+
+     public Circle(double radius) {
+        this.radius = (radius < 0) ? 0 : radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+         return radius * radius * Math.PI;
+    }
+
+}
---
+++ Section 6/Cylinder/Cylinder/src/academy/learnprogramming/Cylinder.java
@@ -0,0 +1,19 @@
+package academy.learnprogramming;
+
+public class Cylinder extends Circle{
+
+    private double height;
+
+    public Cylinder(double height, double radius) {
+        super(radius);
+        this.height = (height < 0) ? 0 : height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return getArea() * height;
+    }
+}
