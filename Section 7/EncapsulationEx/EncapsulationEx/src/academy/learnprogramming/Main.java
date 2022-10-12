14284f2bbd6b034c1e2429be70a161fcfca5b1bd
Ivan Logutov <foreverivan2004@gmail.com>
2022-10-12 04:33:10 +0300

    Adding Coding Exercise 38

--- Section 7/EncapsulationEx/EncapsulationEx/src/academy/learnprogramming/Main.java
+++ Section 7/EncapsulationEx/EncapsulationEx/src/academy/learnprogramming/Main.java
@@ -2,6 +2,12 @@ package academy.learnprogramming;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Printer printer = new Printer(50, true);
+        System.out.println(printer.addToner(50));
+        System.out.println("initial page count = " +printer.getPagesPrinted());
+        int pagesPrinted = printer.printPages(4);
+        System.out.println("Pages printed was " + pagesPrinted +" new total print count for printer = " +printer.getPagesPrinted());
+        pagesPrinted = printer.printPages(2);
+        System.out.println("Pages printed was " + pagesPrinted +" new total print count for printer = " +printer.getPagesPrinted());
     }
 }
