42b895bc486cb408f4250ed841c8f3d3d5955ec1
Ivan Logutov <foreverivan2004@gmail.com>
2023-05-19 20:37:16 +0200

    adding InnerClass Challenge (meal and store challenge)

---
+++ Section 10/InnerClassChallenge/InnerClassChallenge/src/burger/Store.java
@@ -0,0 +1,16 @@
+package burger;
+
+public class Store {
+
+    public static void main(String[] args) {
+
+        Meal regularMeal = new Meal();
+        regularMeal.addToppings("Ketchup","Bacon", "Cheddar");
+        System.out.println(regularMeal);
+
+        Meal USRegularMeal = new Meal(0.68);
+        System.out.println(USRegularMeal);
+
+    }
+}
+
